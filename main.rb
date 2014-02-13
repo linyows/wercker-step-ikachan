0a3a9b2bf0829f4a5b7f60a86a048d88861e1e26
linyows <linyows@gmail.com>
2014-02-13 16:16:40 +0900

    fix color

--- main.rb
+++ main.rb
@@ -6,6 +6,12 @@ require 'uri'
 host, port, channel, message = ARGV
 params = { 'channel' => "##{channel}" }
 
+colored_message = message.gsub(/(Wercker \(.*\):)/, "\x0308\\1\x0f").
+  gsub(/(build)/, "\x02\\1\x0f").
+  gsub(/(deploy)/, "\x0302\\1\x0f").
+  gsub(/(failed)\./, "\x0304\\1\x0f").
+  gsub(/(passed)\./, "\x0303\\1\x0f")
+
 uri = URI("http://#{host}:#{port}/join")
 http = Net::HTTP.new(uri.host, uri.port)
 req = Net::HTTP::Post.new(uri.path)
@@ -15,5 +21,5 @@ http.request(req)
 uri = URI("http://#{host}:#{port}/notice")
 http = Net::HTTP.new(uri.host, uri.port)
 req = Net::HTTP::Post.new(uri.path)
-req.form_data = params.merge('message' => message)
+req.form_data = params.merge('message' => colored_message)
 http.request(req)
