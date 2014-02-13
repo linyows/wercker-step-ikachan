6f2da21df5fe77133a879d03a326561544aa8263
linyows <linyows@gmail.com>
2014-02-13 17:16:09 +0900

    support random color

--- main.rb
+++ main.rb
@@ -2,15 +2,20 @@
 
 require 'net/http'
 require 'uri'
+require 'digest/md5'
 
-host, port, channel, message = ARGV
+host, port, channel, message, repo = ARGV
 params = { 'channel' => "##{channel}" }
 
-colored_message = message.gsub(/(Wercker \(.*\):)/, "\x0308\\1\x0f").
+magic_number = Digest::MD5.hexdigest(repo).gsub(/[^0-9]/, '').split('').last.to_i
+number = "%02d" % (5..14).to_a[magic_number]
+
+colored_message = message.
+  gsub(/(^.*):/, "\x03#{number}\\1\x0f:").
   gsub(/(build)/, "\x02\\1\x0f").
   gsub(/(deploy)/, "\x0302\\1\x0f").
-  gsub(/(failed)\./, "\x0304\\1\x0f").
-  gsub(/(passed)\./, "\x0303\\1\x0f")
+  gsub(/(failed)/, "\x0304\\1\x0f").
+  gsub(/(passed)/, "\x0303\\1\x0f")
 
 uri = URI("http://#{host}:#{port}/join")
 http = Net::HTTP.new(uri.host, uri.port)
